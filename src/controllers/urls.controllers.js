dce2f5edf5d07b34afde7a6fec48312ec5e1828a
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-08-03 11:21:55 -0300

    feat: redirect shorten url

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -8,16 +8,16 @@ export async function newUrl(req, res) {
     const authorization = req.headers.authorization;
     if (!authorization) return res.sendStatus(401);
 
-    const shortUrl = nanoid(8);
+    const short_url = nanoid(8);
 
     const insertUrlQuery =
-      "INSERT INTO urls (url, shortUrl) VALUES ($1, $2) RETURNING id";
-    const insertUrlValues = [url, shortUrl];
+      "INSERT INTO urls (url, short_url) VALUES ($1, $2) RETURNING id";
+    const insertUrlValues = [url, short_url];
     const insertUrlResult = await db.query(insertUrlQuery, insertUrlValues);
 
     const id = insertUrlResult.rows[0].id;
 
-    res.status(201).json({ id, shortUrl });
+    res.status(201).json({ id, short_url });
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -26,16 +26,47 @@ export async function newUrl(req, res) {
 export async function getUrlById(req, res) {
   const { id } = req.params;
   try {
-    const query = "SELECT id,url,shorturl FROM urls WHERE id = $1";
+    const query = "SELECT id,url,short_url FROM urls WHERE id = $1";
     const values = [id];
     const result = await db.query(query, values);
 
     if (result.rows.length === 0) return res.sendStatus(404);
 
     const urlData = result.rows[0];
-    const { shortUrl, url } = urlData;
-    res.status(200).json({ id, url, shortUrl });
+    const { short_url, url } = urlData;
+    res.status(200).json({ id, url, short_url });
   } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function getShortUrl(req, res) {
+  const { short_url } = req.params;
+
+  try {
+    console.log("getShortUrl: Start");
+    console.log("getShortUrl: Short URL:", short_url);
+    const query = "SELECT url FROM urls WHERE short_url = $1";
+    const values = [short_url];
+    const result = await db.query(query, values);
+    console.log("getShortUrl: Database Query Result:", result.rows);
+
+    if (result.rows.length === 0) {
+      console.log("getShortUrl: Short URL not found in database");
+      return res.sendStatus(404);
+    }
+    const url = result.rows[0].url;
+    console.log("getShortUrl: Original URL:", url);
+
+    const updateViewsQuery =
+      "UPDATE urls SET views = views + 1 WHERE short_url = $1";
+    await db.query(updateViewsQuery, [short_url]);
+    console.log("getShortUrl: Redirecting to URL:", url);
+
+    res.redirect(url);
+  } catch (err) {
+    console.error("getShortUrl: Error:", err.message);
+
     res.status(500).send(err.message);
   }
 }
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { schemaValidation } from "../middlewares/schema.validation.js";
 import schemaUrl from "../schemas/url.schemas.js";
-import { getUrlById, newUrl } from "../controllers/urls.controllers.js";
+import {
+  getShortUrl,
+  getUrlById,
+  newUrl,
+} from "../controllers/urls.controllers.js";
 
 const urlsRoutes = Router();
 
 urlsRoutes.post("/urls/shorten", schemaValidation(schemaUrl), newUrl);
 urlsRoutes.get("/urls/:id", getUrlById);
-urlsRoutes.get("/urls/open/:shorturl");
+urlsRoutes.get("/urls/open/:short_url", getShortUrl);
 urlsRoutes.delete("/urls/:id");
 
 export default urlsRoutes;
